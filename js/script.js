5f8850be81960fdf274b99312f0ed355a5354656
worldofoats <Worldofoats@proton.me>
2026-09-19 20:01:27 -0400

    yay javascript on its own file now

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+function buttonGoto(x){
+
+          switch(x) {
+            case 'adoption': window.open('https://mabsland.com/Adoption.html')
+            break;
+            case 'fidget':window.open('https://fidgetcube.dev/')
+            break;
+            case 'fuckai':window.open('https://88x31.datakra.sh/img/random/fuckai.gif')
+            break;
+            case 'gay':window.open('https://pillowfort.social/posts/1632320')
+            break;
+            case 'hrt':window.open('https://alyx.sh/posts/hrt-geocities-buttons/')
+            break;
+            case 'kate':window.open('https://88x31.kate.pet/')
+            break;
+            case 'hyena':window.open('https://hyenatown.nekoweb.org/')
+            break;
+            case 'cyber':window.open('https://cyber.dabamos.de/88x31/')
+            break;
+            case 'sgc':window.open('https://sadgirlsclub.wtf/')
+            break;
+            case 'feed':window.open('/feed.xml')
+            break;
+            case 'mini':window.location.href = "/blogs/mini.html"
+            break;
+            case 'blog':window.location.href = "/blog.html"
+            break;
+            default:window.open('https://sadgirlsclub.wtf/')
+          }
+        }
+
+      function goTo(x) {
+          y = window.location.pathname
+          window.location = y+"#"+x
+         //window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#myAnchor"
+         /* switch(x) {
+            case 'shelter': open('#shelter')
+            break;
+            case 'escape from blue marble':
+            break;
+            case 'eusexua': 
+            break;
+            case 'unicorns are real': 
+            break;
+            case 'constant companions': 
+            break; */
+          }
